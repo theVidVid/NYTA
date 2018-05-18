aad58181cec092face1cb8d61a695367db2c96ba
Ian Vidaurre <ianjvidaurre@gmail.com>
2018-05-17 23:19:14 -0400

    I fell on my Heroku sword.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/index'
+  get 'admins/index'
   devise_for :members
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
